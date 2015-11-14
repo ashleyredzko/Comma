e5c8caa547a98fc8c3c228ccae02d672a92a3116
Tyler Redzko <tylerredzko@gmail.com>
2015-11-14 17:13:54 -0500

    Binary image test - failing

--- apicall.py
+++ apicall.py
@@ -2,14 +2,19 @@ import requests
 import json
 
 headers = {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/octet-stream',
     'Ocp-Apim-Subscription-Key': 'e161d0e1d12c434d8b00949bebc9da60'
 }
-picture = 'https://s.aolcdn.com/hss/storage/midas/fedc672e42fb3f2457bb4a41321a21e7/200006428/smiling-women.jpg'
-url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
+
+picture = 'https://theparentescape.files.wordpress.com/2015/10/prevention-image-of-large-group-smiling-300x200.jpg'
+
 data = {
     'url': picture
 }
-r = requests.post(url, data=json.JSONEncoder().encode(data), headers=headers)
+
+url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
+files = {'files': open('example.jpg', 'rb').read() }
+
+r = requests.post(url, data=files, headers=headers)
 
 print(r.text)
