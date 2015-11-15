4a6585af396975335baa3f0eafd5f7239fa9ddea
Brian Grohe <grohe43@gmail.com>
2015-11-15 06:08:07 -0500

    Added import tool for movie average data
    updated README to have proper schema
    Set git to ignore .db files

---
+++ import_movie.py
@@ -0,0 +1,23 @@
+import sqlite3
+from decimal import *
+
+database_connection = sqlite3.connect('comma_database.db')
+
+cursor = database_connection.cursor()
+filename = input('Please input the filename: ')
+moviename = filename.split('.')
+file = open("parseable/" + filename, "r")
+row_data = {'name': moviename[0]}
+
+for line in file:
+	tokenized_list = line.split(' ')
+	row_data[tokenized_list[0]  + '_amt'] = tokenized_list[2]
+	
+columns = ', '.join(row_data.keys())
+placeholders = ', '.join('?' * len(row_data))
+sql = 'INSERT INTO  movies ({}) VALUES ({})'.format(columns, placeholders)
+print(sql, list(row_data.values()))
+cursor.execute(sql, list(row_data.values()))
+
+database_connection.commit()
+database_connection.close()
