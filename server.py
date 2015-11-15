f048f6d4e1f67e26347fb5f0dc3db8c349eb531f
Kevin Brown <kevin@kevin-brown.com>
2015-11-15 09:51:05 -0500

    Set up the other three main pages
    Now they will be served at some custom urls with Flask and Jinja for
    doing the templates

--- server.py
+++ server.py
@@ -6,6 +6,18 @@ app = Flask(__name__, static_folder="static", static_url_path="")
 def home():
     return render_template('Home.html')
 
+@app.route('/demo')
+def demo():
+    return render_template('Demo.html')
+
+@app.route('/about')
+def about():
+    return render_template('About.html')
+
+@app.route('/team')
+def team():
+    return render_template('Team.html')
+
 if __name__ == "__main__":
     app.config["DEBUG"] = True
     app.run(host="0.0.0.0", port=8000)
