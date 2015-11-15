498e8664e1eaa1f538f840c8c8108f7f08b953f4
Kevin Brown <kevin@kevin-brown.com>
2015-11-15 00:46:13 -0500

    Handle extra frames
    There can be up to 5 extra frames that normally wouldn't be saved, this
    saves them at the end.

--- frames.py
+++ frames.py
@@ -17,33 +17,36 @@ y = 0
 
 while video.isOpened() and more_frames:
     more_frames, frame = video.read()
-    
+
     if frame_number % 2 == 0:
         if frame is None or not frame.any():
             more_frames = False
             continue
-        
+
         if framesx is None:
             framesx = frame
         else:
             framesx = np.concatenate((framesx, frame), axis=0)
-        
+
         if frame_number % 5 == 0:
-            
+
             if framesy is None:
                 framesy = framesx
             else:
                 framesy = np.concatenate((framesy, framesx), axis=1)
-                
+
             y += 1
-                
+
             if y == 2:
                 cv2.imwrite('frames/frames_%d.jpg' % frame_number, framesy)
                 framesy = None
                 y = 0
-                
+
             framesx = None
 
     frame_number += 1
+
+if framesy is not None:
+    cv2.imwrite('frames/frames_%d.jpg' % frame_number, framesy)
 
 video.release()
